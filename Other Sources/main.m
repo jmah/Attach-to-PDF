d86d3e1b071923a0b4f9d6cf58cda416874457ef
Jonathon Mah <me@JonathonMah.com>
2008-02-10 01:29:41 +1030

    Initial commit of skeleton project

---
+++ Other Sources/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Attach to PDF
+//
+//  Created by Jonathon Mah on 2008-02-10.
+//  Copyright Playhaus 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
